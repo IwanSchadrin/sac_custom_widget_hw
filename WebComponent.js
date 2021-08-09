5a7be16e10058d400c771dd51760175d82d3bc07
IwanSchadrin <44634607+IwanSchadrin@users.noreply.github.com>
2021-08-09 17:07:06 +0200

    Update WebComponent.js

--- WebComponent.js
+++ WebComponent.js
@@ -4,7 +4,7 @@
         <h1>Hello World from Sir Ivan</h1>
     `;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+    customElements.define('com-promos-is-helloworld1', class HelloWorld1 extends HTMLElement {
 
 
 		constructor() {
