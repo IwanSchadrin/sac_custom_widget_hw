fe403a862238ca82a45415b68ef9f65b0599d65c
IwanSchadrin <44634607+IwanSchadrin@users.noreply.github.com>
2021-08-10 09:55:23 +0200

    Update WebComponent.js

--- WebComponent.js
+++ WebComponent.js
@@ -1,41 +1,44 @@
-(function()  {
+(function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello World from Sir Ivan</h1>
+        
     `;
 
-    customElements.define('com-promos-is-helloworld-step1', class HelloWorld extends HTMLElement {
+    customElements.define('com-promos-is-helloworld-step2', class HelloWorld extends HTMLElement {
         constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
-		}
+            super();
+            let shadowRoot = this.attachShadow({ mode: "open" });
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._tagContainer;
+            this._tagType = "h1";
+            this._tagText = "Hello World";
+        }
 
         //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
+        connectedCallback() {
             this._firstConnection = true;
             this.redraw();
         }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
+        //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback() {
+
         }
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function first
         onCustomWidgetBeforeUpdate(oChangedProperties) {
 
-		}
+        }
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
         onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection){
+            if (this._firstConnection) {
                 this.redraw();
             }
         }
-        
+
         //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
+        onCustomWidgetDestroy() {
         }
 
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
@@ -47,9 +50,21 @@
         }
         */
 
-        redraw(){
+        redraw() {
+            if (this._tagText != null){
+                if (this._tagContainer){
+                    this._tagContainer.parentNode.removeChild(this._tagContainer);
+                }
+        
+                var shadow = window.getSelection(this._shadowRoot);
+                
+                this._tagContainer = document.createElement(this._tagType);
+                var theText = document.createTextNode(this._tagText);    
+                this._tagContainer.appendChild(theText); 
+                this._shadowRoot.appendChild(this._tagContainer);
+            }
         }
-        
+
     });
-        
+
 })();
